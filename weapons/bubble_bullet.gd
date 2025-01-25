2cfc2cabe04a20ffd5b7f8786e01186f0e87d187
Eric Gentry <egtwobits@gmail.com>
2025-01-25 15:47:13 -0500

    Add bubble bullet collision.

--- weapons/bubble_bullet.gd
+++ weapons/bubble_bullet.gd
@@ -4,4 +4,9 @@ var MAX_SPEED = 10
 
 
 func _physics_process(delta):
-	move_and_slide()
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		var collider = collision_info.get_collider()
+		if collider:
+			print('HIT PLAYER')
+			call_deferred('queue_free')
